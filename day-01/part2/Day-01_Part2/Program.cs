f03c00cbeabf7192caead87aa43ea2e64d325004
Stefan Stangl <stangl.stefan@gmail.com>
2018-12-01 19:56:22 +0100

    day-01 done

---
+++ day-01/part2/Day-01_Part2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day_01_Part2
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         HashSet<int> freqs = new HashSet<int>();
+
+         int freq = 0;
+
+         freqs.Add(freq);
+
+         bool found = false;
+         while (!found)
+         {
+            foreach (string line in File.ReadLines("input.txt"))
+            {
+               freq += int.Parse(line);
+
+               if (freqs.Contains(freq))
+               {
+                  Console.WriteLine(freq);
+                  found = true;
+                  break;
+               }
+               else
+               {
+                  freqs.Add(freq);
+               }
+            }
+         }
+
+         Console.Read();
+      }
+   }
+}
